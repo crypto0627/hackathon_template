4e3d7d28b65168c10f25cb4e24a057487e917144
crypto0627 <jake0627a1@gmail.com>
2023-11-17 11:33:14 +0800

    fix: monorepe

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,11 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
-  transpilePackages: ["ui"],
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
 };
+
+module.exports = nextConfig;
